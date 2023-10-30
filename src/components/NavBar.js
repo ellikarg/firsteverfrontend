c0322471319190055173558b3332b8b1fbd46d89
elli karg <elisa.karg.bogor@gmail.com>
2023-10-30 11:37:08 +0000

    feat: improve usx for navbar toggle functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,11 +7,14 @@ import { NavLink } from "react-router-dom";
 import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext';
 import Avatar from './Avatar';
 import axios from 'axios';
+import useClickOutsideToggle from '../hooks/useClickOutsideToggle';
 
 const NavBar = () => {
 
     const currentUser = useCurrentUser();
     const setCurrentUser = useSetCurrentUser();
+
+    const {expanded, setExpanded, ref} = useClickOutsideToggle();
 
     const handleSignOut = async () => {
         try {
@@ -55,7 +58,7 @@ const NavBar = () => {
         </>
 
     return (
-        <Navbar className={styles.NavBar} expand="md" fixed="top">
+        <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
             <Container>
                 <NavLink exact to="/">
                     <Navbar.Brand>
@@ -63,7 +66,10 @@ const NavBar = () => {
                     </Navbar.Brand>
                 </NavLink>
                 {currentUser && addPostIcon}
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Toggle
+                    ref={ref}
+                    onClick={() => setExpanded(!expanded)} 
+                    aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-right">
                         <NavLink exact to="/">
