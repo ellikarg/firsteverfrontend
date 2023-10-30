bca7d9f649745c1b61c02f90f95a2cfc6f2d4fec
elli karg <elisa.karg.bogor@gmail.com>
2023-10-30 11:13:06 +0000

    feat: add loggedIn Navbar icons with avatar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,24 +4,71 @@ import logo from '../assets/logo4.png'
 import "bootstrap/js/src/collapse.js";
 import styles from '../styles/NavBar.module.css'
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from '../contexts/CurrentUserContext';
+import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext';
+import Avatar from './Avatar';
+import axios from 'axios';
 
 const NavBar = () => {
 
     const currentUser = useCurrentUser();
-    const loggedInIcons = <>{currentUser?.username}</>
+    const setCurrentUser = useSetCurrentUser();
+
+    const handleSignOut = async () => {
+        try {
+            await axios.post("dj-rest-auth/logout/");
+            setCurrentUser(null);
+        } catch(err) {
+            console.log(err)
+        }
+    };
+
+    const addPostIcon = (
+        <NavLink to="/posts/create">
+            <i className="fa-solid fa-plus-square"></i>Add Post
+        </NavLink>
+    )
+
+    const loggedInIcons = 
+        <>
+        <NavLink to="/posts/feed">
+            <i className="fa-solid fa-stream"></i>Feed
+        </NavLink>
+        <NavLink to="/posts/liked">
+            <i className="fa-solid fa-heart"></i>Liked
+        </NavLink>
+        <NavLink to="/" onClick={handleSignOut}>
+            <i className="fa-solid fa-sign-out-alt"></i>Sign out
+        </NavLink>
+        <NavLink to={`/profiles/${currentUser?.profile_id}`}>
+            <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+        </NavLink>
+        </>
+
     const loggedOutIcons = 
-        <><NavLink to="/signin"><i className="fa-solid fa-right-to-bracket"></i>Sign in</NavLink>
-            <NavLink to="/signup"><i className="fa-solid fa-user-plus"></i>Sign up</NavLink></>
+        <>
+            <NavLink to="/signin">
+                <i className="fa-solid fa-right-to-bracket"></i>Sign in
+            </NavLink>
+            <NavLink to="/signup">
+                <i className="fa-solid fa-user-plus"></i>Sign up
+            </NavLink>
+        </>
 
     return (
         <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
-                <Navbar.Brand><img src={logo} alt="logo" height={80}></img></Navbar.Brand>
+                <NavLink exact to="/">
+                    <Navbar.Brand>
+                        <img src={logo} alt="logo" height={80}></img>
+                    </Navbar.Brand>
+                </NavLink>
+                {currentUser && addPostIcon}
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-right">
-                        <NavLink exact to="/"><i className="fas fa-home"></i>Home</NavLink>
+                        <NavLink exact to="/">
+                            <i className="fas fa-home"></i>Home
+                        </NavLink>
                         {currentUser ? loggedInIcons : loggedOutIcons}
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
