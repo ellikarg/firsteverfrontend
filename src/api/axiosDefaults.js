57043601b63bf53eb2a102ba2c695b3150241f56
elli karg <elisa.karg.bogor@gmail.com>
2023-10-30 10:00:25 +0000

    debug: thorough check of Context file for debug

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://firsteverbackend-391b6fc97a98.herokuapp.com/';
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.baseURL = "https://firsteverbackend-391b6fc97a98.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -2,23 +2,23 @@ import { createContext, useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 
 
-export const CurrentUserContext = createContext()
-export const SetCurrentUserContext = createContext()
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
 
 export const useCurrentUser = () => useContext(CurrentUserContext);
 export const useSetCurrentUser = () => useContext(SetCurrentUserContext);
 
-export const CurrentUserProvider = ({children}) => {
-    const [currentUser, setCurrentUser] = useState(null)
+export const CurrentUserProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(null);
 
     const handleMount = async () => {
         try {
-            const {data} = await axios.get('/dj-rest-auth/user/')
-            setCurrentUser(data)
+            const { data } = await axios.get("dj-rest-auth/user/");
+            setCurrentUser(data);
         } catch (err) {
-            console.log(err)
+            console.log(err);
         }
-    }
+        };
 
     useEffect(() => {
         handleMount()
@@ -30,5 +30,5 @@ export const CurrentUserProvider = ({children}) => {
             {children}
         </SetCurrentUserContext.Provider>
         </CurrentUserContext.Provider>
-    )
+    );
 };
