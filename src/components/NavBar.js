d95f0e0faee6aa4dc6074f72e6c83b057bd3dbcd
elli karg <elisa.karg.bogor@gmail.com>
2023-11-02 12:51:12 +0000

    style: adjust spacing between navbar icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -92,13 +92,6 @@ const NavBar = () => {
                             <i className="fas fa-home"></i>Home
                         </NavLink>
                         {currentUser ? loggedInIcons : loggedOutIcons}
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider/>
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
