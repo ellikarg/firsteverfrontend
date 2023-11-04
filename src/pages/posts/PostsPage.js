46aa7a37194e7357565d74116c716a09e012ec24
elli karg <elisa.karg.bogor@gmail.com>
2023-11-04 11:31:01 +0000

    debug: CamelCase for fetchMoreData function

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -14,7 +14,7 @@ import NoResults from "../../assets/no_results.PNG"
 import Asset from "../../components/Assets";
 import { Form } from "react-bootstrap";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { FetchMoreData } from "../../utils/utils";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostsPage({message, filter=""}) {
   const [posts, setPosts] = useState({results: []});
@@ -63,15 +63,14 @@ function PostsPage({message, filter=""}) {
           <>
             {posts.results.length ? (
               <InfiniteScroll
-                children={
-                  posts.results.map((post) => (
-                    <Post key={post.id} {...post} setPosts={setPosts} />
-                  ))
+                children={posts.results.map((post) => (
+                  <Post key={post.id} {...post} setPosts={setPosts} />
+                ))
                 }
                 dataLength={posts.results.length}
                 loader={<Asset spinner />}
                 hasMore={!!posts.next}
-                next={() => FetchMoreData(posts, setPosts)}
+                next={() => fetchMoreData(posts, setPosts)}
                 />
             ) : (
               <Container className={appStyles.Content}>
