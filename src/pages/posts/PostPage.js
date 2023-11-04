431ced2be8496c1bad95c768ecf8608db230e64d
elli karg <elisa.karg.bogor@gmail.com>
2023-11-04 09:59:52 +0000

    feat: include the comment feature in the PostPage

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -9,19 +9,28 @@ import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Comment from "../comments/Comment";
+
 function PostPage() {
   
     const { id } = useParams();
     const [post, setPost] = useState({ results: [] });
 
+    const currentUser = useCurrentUser();
+    const profile_image = currentUser?.profile_image;
+    const [comments, setComments] = useState({ results: [] });
+
     useEffect(() => {
         const handleMount = async () => {
             try {
-                const [{ data: post }] = await Promise.all([
+                const [{data: post}, {data: comments}] = await Promise.all([
                     axiosReq.get(`/posts/${id}`),
+                    axiosReq.get(`/comments/?post=${id}`),
                 ]);
                 setPost({ results: [post] });
-                console.log(post);
+                setComments(comments);
             } catch(err) {
                 console.log(err);
             }
@@ -35,7 +44,26 @@ function PostPage() {
             <p>Popular profiles for mobile</p>
             <Post {...post.results[0]} setPosts={setPost} postPage />
             <Container className={appStyles.Content}>
-            Comments
+                {currentUser ? (
+                    <CommentCreateForm
+                        profile_id={currentUser.profile_id}
+                        profileImage={profile_image}
+                        post={id}
+                        setPost={setPost}
+                        setComments={setComments}
+                    />
+                ) : comments.results.length ? (
+                    "Comments"
+                ) : null}
+                {comments.results.length ? (
+                    comments.results.map((comment) => (
+                    <Comment key={comment.id} {...comment} />
+                    ))
+                ) : currentUser ? (
+                    <span>No comments yet, be the first to comment!</span>
+                ) : (
+                    <span>No comments... yet</span>
+                )}
             </Container>
         </Col>
         <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
