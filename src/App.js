10aa4f74ccbc73755d56e2967ad517b1a1840918
elli karg <elisa.karg.bogor@gmail.com>
2023-11-05 20:10:42 +0000

    feat: add forms to app.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ import PostsPage from './pages/posts/PostsPage';
 import { useCurrentUser } from './contexts/CurrentUserContext';
 import PostEditForm from './pages/posts/PostEditForm';
 import ProfilePage from './pages/profiles/ProfilePage';
+import UsernameForm from "./pages/profiles/UsernameForm";
+import UserPasswordForm from "./pages/profiles/UserPasswordForm";
+import ProfileEditForm from "./pages/profiles/ProfileEditForm";
 
 
 function App() {
@@ -23,24 +26,44 @@ function App() {
             <NavBar/>
             <Container className={styles.Main}>
                 <Switch>
-                    <Route exact path="/" render={() => (<PostsPage
-                            message="Sorry, no results found."/>)}/>
-                    <Route exact path="/feed" render={() => (<PostsPage
-                            message="Sorry, no results found. Adjust the search keyword or follow a user."
-                            filter={`owner__followed__owner__profile=${profile_id}&`}/>)}/>
-                    <Route exact path="/liked" render={() => (<PostsPage
-                            message="Sorry, no results found. Adjust the search keyword or like a post."
-                            filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}/>)}/>
-                    <Route exact path="/signin" render={() => <SignInForm />}/>
-                    <Route exact path="/signup" render={() => <SignUpForm />}/>
-                    <Route exact path="/posts/create"
+                    <Route 
+                        exact path="/" render={() => (<PostsPage
+                        message="Sorry, no results found."/>)}/>
+                    <Route 
+                        exact path="/feed" render={() => (<PostsPage
+                        message="Sorry, no results found. Adjust the search keyword or follow a user."
+                        filter={`owner__followed__owner__profile=${profile_id}&`}/>)}/>
+                    <Route 
+                        exact path="/liked" render={() => (<PostsPage
+                        message="Sorry, no results found. Adjust the search keyword or like a post."
+                        filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}/>)}/>
+                    <Route 
+                        exact path="/signin" render={() => <SignInForm />}/>
+                    <Route 
+                        exact path="/signup" render={() => <SignUpForm />}/>
+                    <Route 
+                        exact path="/posts/create"
                         render={() => <PostCreateForm/>}/>
-                    <Route exact path="/posts/:id/edit"
+                    <Route 
+                        exact path="/posts/:id/edit"
                         render={() => <PostEditForm />}/>
-                    <Route exact path="/posts/:id"
+                    <Route 
+                        exact path="/posts/:id"
                         render={() => <PostPage/>}/>
                     <Route exact path="/profiles/:id"
                         render={() => <ProfilePage/>}/>
+                    <Route
+                        exact
+                        path="/profiles/:id/edit/username"
+                        render={() => <UsernameForm />}/>
+                    <Route
+                        exact
+                        path="/profiles/:id/edit/password"
+                        render={() => <UserPasswordForm />}/>
+                    <Route
+                        exact
+                        path="/profiles/:id/edit"
+                        render={() => <ProfileEditForm />}/>
                     <Route render={() => <p>Page not found!</p> }/>
                 </Switch>
         </Container>
