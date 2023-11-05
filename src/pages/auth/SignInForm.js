1af104a99d370200735b0f0bf118258da6095a5f
elli karg <elisa.karg.bogor@gmail.com>
2023-11-05 21:00:36 +0000

    add jwt-decode for TokenTimestamp handling

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -4,6 +4,7 @@ import { useHistory } from "react-router-dom";
 import axios from "axios";
 import { useSetCurrentUser } from '../../contexts/CurrentUserContext';
 import { useRedirect } from '../../hooks/useRedirect';
+import { setTokenTimestamp } from '../../utils/utils';
 
 function SignInForm() {
     const setCurrentUser = useSetCurrentUser();
@@ -29,6 +30,7 @@ function SignInForm() {
         try {
           const {data} = await axios.post("/dj-rest-auth/login/", signInData);
           setCurrentUser(data.user);
+          setTokenTimestamp(data);
           history.goBack();
         } catch (err) {
             setErrors(err.response?.data);
