10ad270094a9515474a1a9496dcb309dd057e230
elli karg <elisa.karg.bogor@gmail.com>
2023-11-05 20:12:30 +0000

    feat: add ProfileEditDropdown component to profilepage

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -19,6 +19,8 @@ import {fetchMoreData} from "../../utils/utils"
 import NoResults from "../../assets/no_results.PNG"
 import Post from "../posts/Post";
 
+import { ProfileEditDropdown } from "../../components/MoreDropdown";
+
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
   const currentUser = useCurrentUser();
@@ -53,6 +55,7 @@ function ProfilePage() {
 
   const mainProfile = (
     <>
+    {profile?.is_owner && <ProfileEditDropdown id={profile?.id} />}
       <Row noGutters className="px-3 text-center">
         <Col lg={3} className="text-lg-left">
           <Image className={styles.ProfileImage}
